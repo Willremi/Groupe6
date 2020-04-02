34d8b0281e09bb5228ceeb1e6bfd3ba30d5b0681
Willremi <willremi@gmail.com>
2020-04-02 14:15:05 +0200

    mise à jour

--- ExoSQL/reponse.sql
+++ ExoSQL/reponse.sql
@@ -2,3 +2,9 @@ SELECT prod.name AS Nom,
        DATE_FORMAT(prod.created, '%Y') AS annéeProduit, 
        prod.price AS Prix
 FROM products AS prod
+
+SELECT prod.name AS Nom, 
+       DATE_FORMAT(prod.created, '%Y') AS annéeProduit, 
+       prod.price AS Prix
+FROM products AS prod
+WHERE prod.category_id = 1
