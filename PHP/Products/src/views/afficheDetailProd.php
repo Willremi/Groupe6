2ce31e58057972027bfab9133492d70eb16ba010
Willremi <willremi@gmail.com>
2020-04-08 17:35:29 +0200

    maj afficheDetailProd.php

--- PHP/Products/src/views/afficheDetailProd.php
+++ PHP/Products/src/views/afficheDetailProd.php
@@ -14,7 +14,8 @@ products.price AS prix,
 categories.name AS categorie, 
 DATE_FORMAT(products.created, '%d/%m/%Y à %H:%i') AS dateAjout
 FROM products
-INNER JOIN categories ON products.category_id = categories.id";
+INNER JOIN categories ON products.category_id = categories.id
+WHERE products.id = 1";
 
 $reqDet = $db->prepare($sqlDetailProd);
 $reqDet->execute();
@@ -26,11 +27,11 @@ $data = $reqDet->fetchObject();
 <div class="card">
     <div class="card-body">
     <p class="card-text">ID du produit : <?= $data->idProd ?></p>
-        <p class="card-text">Nom du description : <?= $data->nom ?></p>
-        <p class="card-text">Description du produit : <?= $data->descrip ?></p>
-        <p class="card-text">Prix : <?= $data->prix ?> €</p>
-        <p class="card-text">Catégorie : <?= $data->categorie ?></p>
-        <p class="card-text">Date d'Ajout : <?= $data->dateAjout ?></p>
+        <p class="card-text">Nom du description : <?= $data->nom; ?></p>
+        <p class="card-text">Description du produit : <?= $data->descrip; ?></p>
+        <p class="card-text">Prix : <?= $data->prix; ?> €</p>
+        <p class="card-text">Catégorie : <?= $data->categorie; ?></p>
+        <p class="card-text">Date d'Ajout : <?= $data->dateAjout; ?></p>
     </div>
 </div>
 <hr>
--- PHP/Products/src/views/modifProd.php
+++ PHP/Products/src/views/modifProd.php
@@ -6,7 +6,35 @@ require '../models/connect.php';
 head();
 
 $db = connect();
+
+$sqlProd = "SELECT products.id AS idProd, 
+products.name AS nom,  
+products.description AS descrip, 
+products.price AS prix,
+categories.name AS categorie 
+FROM products
+INNER JOIN categories ON products.category_id = categories.id";
+
+$reqDet = $db->prepare($sqlProd);
+$reqDet->execute();
+$data = $reqDet->fetchObject();
+
+$sqlCat = "SELECT * FROM categories";
+$reqCat = $db->prepare($sqlCat);
+$reqCat->execute();
+
 ?>
+<h2>Modifier votre produit</h2>
+<form action="" method="get">
+    <div class="form-group">
+    <label for="idProd">ID du produit : </label>
+    <input type="text" name="idProd" id="idProd" class="d-none" value="<?= $data->idProd; ?>"><?= $data->idProd; ?>
+    </div>
+    <!-- <div class="form-group">
+        <label for="nomProd">Nom du produit :</label>
+        <input type="text" name="nomProd" id="nomProd" class="d-none" value="<?= $data->nom; ?>"><?= $data->nom; ?>
+    </div> -->
+</form>
 
 <hr>
 <a href="../../products.php"><button type="button" class="btn btn-danger">Retour</button></a>
