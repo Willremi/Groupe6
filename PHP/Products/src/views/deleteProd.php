5a29a908a45c89e4e968e09f9c5d04364cda91aa
Willremi <willremi@gmail.com>
2020-04-09 20:06:24 +0200

    maj deleteProd.php

--- PHP/Products/src/views/deleteProd.php
+++ PHP/Products/src/views/deleteProd.php
@@ -4,20 +4,33 @@ include_once 'elements/footer.php';
 require '../config/config.php';
 require '../models/connect.php';
 
-head();
+// head();
 $db = connect();
 
-$id = $_GET['id'].'<br>';
+$id = $_GET['id'];
+var_dump($id);
 
+if($_POST['positive']){
+    // echo "vous avez utilisé le bouton1";
+    $sql = "DELETE FROM products WHERE id = :idDel LIMIT 1";
+    $reqDEl = $db->prepare($sql);
+    $reqDEl->bindParam(":idDel", $id);
+    $reqDEl->execute();
+    // header('Location: ../../products.php');
+    
+    }
+    elseif($_POST['negative'])
+    {
+    header('Location: ../../products.php');
+    }
 ?>
 <p>Voulez vous supprimez ce produit ?</p>
-<button type="submit" class="btn btn-success" value="oui">OUI</button>
-<a href="../../products.php"><button type="submit" class="btn btn-danger">NON</button></a>
-<?php 
-// if () {
 
-// }
-?>
+<form action="deleteProd.php" method="post">
+<button type="submit" class="btn btn-success" value="oui" name="positive">OUI</button>
+<a href="Location: ../../products.php"><button type="submit" class="btn btn-danger" name="negative" value="non">NON</button></a>
+</form>
+
 <hr>
 <a href="../../products.php"><button type="button" class="btn btn-secondary">Retour</button></a>
 <?php
