87662f346489bfb435709666c464777f7ecf59c7
Willremi <willremi@gmail.com>
2020-04-16 08:14:06 +0200

    maj  contact.php

--- PHP/projetImmo/src/views/mail.php
+++ PHP/projetImmo/src/views/mail.php
@@ -9,6 +9,15 @@ head();
 $db = connect();
 
 
+// Le message
+$message = "Line 1\r\nLine 2\r\nLine 3";
+
+// Dans le cas où nos lignes comportent plus de 70 caractères, nous les coupons en utilisant wordwrap()
+$message = wordwrap($message, 70, "\r\n");
+
+// Envoi du mail
+mail('willremi@gmail.com', 'Mon Sujet', $message);
+
 
 // if (isset($_POST['message'])) {
 //         $position_arobase = strpos($_POST['_replyto'], '@');
