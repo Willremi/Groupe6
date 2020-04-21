783de399ae00f10b1537608dbb0fcea90cfe200e
Willremi <willremi@gmail.com>
2020-04-21 15:45:43 +0200

    maj style.css et ajout main.js

---
+++ projet/site/public/js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Pour chaque tableau table
+        // Je collecte les labels
+    document.querySelectorAll('.table-respo').forEach(function (table) {
+        let labels = Array.from(table.querySelectorAll('th')).map(function(th) {
+            return th.innerText;
+        });
+        // Pour chaque td dans table
+            // On récupère l'index du td
+            // On va mettre le data-label qui correspond
+        table.querySelectorAll('td').forEach(function (td, i) {
+             td.setAttribute('data-label', labels[i % labels.length]);
+             
+        });
+    });
+     
+            
+        
+
+});
