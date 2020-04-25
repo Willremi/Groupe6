f602c0787bcffa80424d22894de07a60b471b97d
Willremi <willremi@gmail.com>
2020-04-25 18:00:51 +0200

    maj mail.php

--- projet/site/src/views/mail.php
+++ projet/site/src/views/mail.php
@@ -20,7 +20,7 @@ if(isset($_POST['sujet'])) {
 if(isset($_POST['message'])) {
     $messageForm = htmlspecialchars(trim($_POST['message']));
 }
-var_dump($messageForm);
+// var_dump($messageForm);
 
 // PHP Mailer
 $mail = new PHPMailer(true);
@@ -28,24 +28,21 @@ $mail = new PHPMailer(true);
 try {
     //Server settings
     // $mail->SMTPDebug = SMTP::DEBUG_SERVER;      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.live.com';     // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;            // Enable SMTP authentication
-    $mail->Username   = 'willremi@live.fr';                     // SMTP username
-    $mail->Password   = 'S9RSaHOgNjz0lWUJ';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;
+    
 
     //Recipients
     $mail->setFrom($email, 'Formulaire de contact');
-    $mail->addAddress('willremi@yahoo.fr', $nom.' '.$prenom);     // Add a recipient
-
+    $mail->addAddress('willremi@yahoo.fr', 'Webmaster du site');     // Add a recipient
+    $mail->addReplyTo($email, $prenom.' '.$nom);
     // Content
     $mail->isHTML(true);    // Set email format to HTML
-    $mail->Subject = $sujet;
-    $mail->Body = $messageForm;
+    $mail->Subject = $sujet; //.' de '.$prenom.' '.$nom;
+    $mail->Body = '<p>'.$messageForm.'</p><br><p>'.$prenom.' '.$nom;
+    
 
     $mail->send();
+    echo '<p>Votre message a été envoyé</p>';
+    echo '<a href="contact.html"><button>Retour</button></a>';
     
 } catch (Exception $e) {
     echo "Le message n'a pas pu être envoyé. Le message d'erreur : {$mail->ErrorInfo}";
