697666b920f10c91e703d2948d7d96fc2277a49a
Willremi <willremi@gmail.com>
2020-04-28 17:10:01 +0200

    ajout dossier apiPays

---
+++ PHP/apiPays/api_nom_pays.php
@@ -0,0 +1,15 @@
+<?php
+
+function showDataByPaysName($nomPays) {
+    $url = file_get_contents("https://restcountries.eu/rest/v2/name/".$nomPays);
+
+    $tab_country = json_decode($url);
+    // var_dump($tab_country[0]->translations->fr);
+
+    
+    foreach($tab_country as $key => $country) {
+        echo $country->translations->fr.'<br>';
+    }
+}
+
+    
