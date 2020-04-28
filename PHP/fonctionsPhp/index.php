72906233e77d7e3c616132370c8bc51ac4f963e9
Willremi <willremi@gmail.com>
2020-04-28 11:32:48 +0200

    suppression vendor pour github

---
+++ PHP/fonctionsPhp/index.php
@@ -0,0 +1,64 @@
+<?php
+$monTexte = "Voici un texte pour ma fonction";
+// echo $monTexte;
+echo '<br>';
+
+$tabString = explode(' ', $monTexte);
+
+// var_dump($tabString);
+
+$nouveauTableau = array();
+
+foreach ($tabString as $index => $string) {
+    $chaine = "";
+    if($index%2) {
+        $chaine .= mb_strtoupper($string);
+    } else {
+        $chaine .= $string;
+    }
+    
+    array_push($nouveauTableau, $chaine);
+}
+
+$newString = implode($nouveauTableau, ' ');
+
+// echo $newString;
+
+// Création de fonction
+$existe = false;
+
+function transformToUpperCase($monString) {
+
+    $tabString = explode(' ', $monString);
+    $nouveauTableau = array();
+
+    foreach ($tabString as $index => $string) {
+        $chaine = "";
+
+        if($index%2) {
+            $chaine .= mb_strtoupper($string);
+        } else {
+            $chaine .= $string;
+        }
+        
+        array_push($nouveauTableau, $chaine);
+    }
+    // $newString = implode($nouveauTableau, ' ');
+    
+    // return $newString;
+    return implode($nouveauTableau, ' ');
+}
+
+function test($ex) {
+    if($ex) {
+        return 'Elle existe';
+    } else {
+        return 'Elle n\'existe pas';
+    }
+    
+}
+
+// Utilisation de la fonction
+echo transformToUpperCase("Voici mon 2 string");
+
+// echo test($existe);
