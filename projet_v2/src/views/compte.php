7947259d6c478f336924d7f51fbaeb786fcecd2c
Willremi <willremi@gmail.com>
2020-05-06 11:11:27 +0200

    maj compte.php

--- projet_v2/src/views/compte.php
+++ projet_v2/src/views/compte.php
@@ -2,11 +2,38 @@
 <?php  
 $login = $_SESSION['login'];
 
-$reqSel = "SELECT * FROM user WHERE pseudoUser = :pseudo";
+$SelUser = "SELECT nomUser AS Nom, 
+                  prenomUser AS Prénom, 
+                  pseudoUser AS Pseudo, 
+                  mailUser AS Email, 
+                  mdpUser AS Mdp, 
+                  numRue AS NumeroRue, 
+                  nomRue1 AS Adresse, 
+                  nomRue2 AS AdresseCompl, 
+                  cpVille AS CodePostal, 
+                  nomVille AS Ville
+           FROM user WHERE pseudoUser = :pseudo";
+
+$reqUser = $db->prepare($SelUser);
+$reqUser->bindParam(':pseudo', $login);
+$reqUser->execute();
+
+$data = $reqUser->fetchObject();
+
 
 ?>
 <div class="container-fluid">
     <h1>Compte utilisateur</h1>
-    <div class="">
+    <div class="offset-sm-5">
+        <p>Nom : <?= $data->Nom ?></p>
+        <p>Prénom : <?= $data->Prénom ?></p>
+        <p>Pseudo : <?= $data->Pseudo ?></p>
+        <p>Email : <?= $data->Email ?></p>
+        <p>Adresse : <?= $data->NumeroRue ?> rue <?= $data->Adresse ?></p>
+        <p>Complément d'adresse : <?= $data->AdressCompl ?></p>
+        <p>Code Postal : <?= $data->CodePostal ?></p>
+        <p>Ville : <?= $data->Ville ?></p>
     </div>
 </div>
+<?php
+// var_dump($data);
