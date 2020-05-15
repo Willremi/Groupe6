9b0480739fbe6760bc3ce5d684754535cda5fa5c
Willremi <willremi@gmail.com>
2020-05-15 10:43:09 +0200

    maj api_pays.php

--- projet_v2/src/api/api_pays.php
+++ projet_v2/src/api/api_pays.php
@@ -1,8 +1,37 @@
 <?php
+switch($nomContinent) {
+    case 'Amériques':
+    $nomContinent = 'Americas';
+    break; 
+    case 'Asie':
+    $nomContinent = 'Asia';
+    break;
+    case 'Afrique':
+    $nomContinent = 'Africa';
+    break;
+    case 'Océanie': 
+    $nomContinent = 'Oceania';
+    break;
+}
+
+/**
+ * showListPays
+ *
+ * @param  mixed $nomContinent
+ * @return void
+ */
 
 function showListPays($nomContinent) {
     //API pour récupérer la liste des pays par continent
-    $url = file_get_contents('https://restcountries.eu/rest/v2/region/'.$nomContinent.'?fields=name;translations');
+    $url = file_get_contents('https://restcountries.eu/rest/v2/region/'.$nomContinent.'?fields=name;translations;alpha2Code');
 
-    $tab = json_decode($url);
+    $tab_list_pays = json_decode($url);
+    
+    foreach($tab_list_pays as $key => $pays) {
+        $paysTrad = $pays->translations;
+        if($paysTrad->fr) {
+            echo $paysTrad->fr.': '.$pays->alpha2Code.'<br>';
+
+        }
+    }
 }
--- projet_v2/src/views/continent.php
+++ projet_v2/src/views/continent.php
@@ -1,6 +1,8 @@
 <?php
 $continent = $_POST['continent'][0];
 $nomContinent = $continent['value'];
-var_dump($nomContinent);
+// var_dump($nomContinent);
 
 require '../api/api_pays.php';
+
+showListPays($nomContinent);
