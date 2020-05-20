a69fdeefac0a47817240fd71540de7e212c2c277
Willremi <willremi@gmail.com>
2020-05-20 12:15:08 +0200

    Update api_acteur.php

--- projet_v2/src/api/api_acteur.php
+++ projet_v2/src/api/api_acteur.php
@@ -17,10 +17,12 @@ function showDetailActeur ($acteur) {
     foreach($tab_acteur as $detailActeur) {
         $idActeur = $detailActeur->id;
         
-        $str= <<<EOD
-        <p>$idActeur</p>
-        
-EOD;
-        echo $str;
+        // var_dump($idActeur);
+        //API pour afficher informations de l'acteur(date et lieu de naissance/ date de décès)
+        $urlCiActeur = file_get_contents('https://api.themoviedb.org/3/person/'.$idActeur.'?api_key=c595147bf4af143ab2df16843f9487bf&language=fr-FR&page=1');
+
+        $tab_ci_acteur = json_decode($urlCiActeur);
+        echo '<p>'.$tab_ci_acteur->birthday.'</p>';
+
     }
 }
