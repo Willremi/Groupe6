49253f4d626586f3d67342083c7794bf54e159b7
Willremi <willremi@gmail.com>
2020-05-20 08:37:24 +0200

    Update acteurs.php

--- projet_v2/src/views/acteurs.php
+++ projet_v2/src/views/acteurs.php
@@ -1,4 +1,13 @@
-<div class="container-fluid">
-    <h1>Page acteurs en construction</h1>
-    Faire la partie acteur, 20/05/2020
+<div class="container">
+    <fieldset>
+        <legend>Rechercher un acteur: </legend>
+        <div class="offset-md-2">
+            <form action="" class="mt-2 form-inline" method="POST" id="formSearch">
+            
+            
+            
+                <button type="submit" class="btn btn-primary" value="Rechercher" id="searchAct">Rechercher</button>
+            </form>
+        </div>
+    </fieldset>
 </div>
