b2982bc8631643962a54aa5978f626ef7e971696
Willremi <willremi@gmail.com>
2020-05-28 13:22:27 +0200

    Update api_saison.php

--- projet_v2/src/api/api_saison.php
+++ projet_v2/src/api/api_saison.php
@@ -26,8 +26,8 @@ function showEpisodeBySaison($idSerie,$saison_number) {
    <hr>
    </div>
    </div>
-   <div class="row justify-content-around">
-   <div class="col-md-4">
+   <div class="row justify-content-between">
+   
    
         
 EOD;
@@ -38,22 +38,29 @@ EOD;
         $saison_number = (int)$saison_number;
         $nbreSaison = $saison->season_number;
         if($nbreSaison === $saison_number) {
-            echo 'toto n°'.$saison->season_number.'<br>';
-            echo $saison->episode_count.' épisodes';
+            echo '<div class="offset-md-1 col-md-3">';
+            echo '<p><strong>'.$saison->name.'</strong></p>';
+            echo '</div>'; // fin col-md-4
+            
+            echo '<div class="col-md-3">';
+            echo '<p><strong>Nombre d\'épisodes :</strong> '.$saison->episode_count.'</p>';
+            echo '</div>';
 
+            $date = $saison->air_date;
+            $date_explode = explode('-', $date);
+            $date_reverse = array_reverse($date_explode);
+            $dateFr = implode('-', $date_reverse);
+
+            echo '<div class="col-md-3">';
+            echo '<p><strong>Date de diffusion :</strong> '.$dateFr.'</p>';
+            echo '</div>';
+            
         }
     }
-
-    echo '</div>'; // fin col-md-4
+    
    echo '</div>'; // fin row justify-content-around
+   echo '<hr>';
+
+
 }
 
-?>
-<!-- <div class="row justify-content-around">
-    <div class="col-md-4">
-        <p><strong>Nombre d'épisodes : </strong></p>
-    </div>
-    <div class="col-md-4">
-      One of two columns
-    </div> 
-</div> -->
