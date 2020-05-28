a22c24f95806e67d6bd227405ccf9417c0527745
Willremi <willremi@gmail.com>
2020-05-28 10:19:58 +0200

    maj api_saison.php

--- projet_v2/src/api/api_saison.php
+++ projet_v2/src/api/api_saison.php
@@ -1,1 +1,23 @@
 <?php
+/**
+ * showEpisodeBySaison
+ *
+ * @param  mixed $idSerie
+ * @param  mixed $saison_number
+ * @return void
+ */
+function showEpisodeBySaison($idSerie,$saison_number) {
+   // API pour récupérer la description de la série
+   $urlSerie = file_get_contents("https://api.themoviedb.org/3/tv/".$idSerie."?api_key=c595147bf4af143ab2df16843f9487bf&language=fr-FR&page=1");
+
+   $tab_select_serie = json_decode($urlSerie);
+
+   // Nom de la série
+   $tab_nom = $tab_select_serie->name;
+   
+   $str=<<<EOD
+   <h1>$tab_nom</h1>
+EOD;
+    echo $str;
+   
+}
--- projet_v2/src/views/saison.php
+++ projet_v2/src/views/saison.php
@@ -1,5 +1,21 @@
 <?php
+require 'src/api/api_saison.php';
+
 $saison_number = $_GET['num'];
 $idSerie = $_GET['idSerie'];
 // var_dump($idSerie);
 
+?>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-12">
+              
+      <?php 
+             if($idSerie !== null && $saison_number !== null) {
+              showEpisodeBySaison($idSerie,$saison_number);
+            } else {
+                echo 'Aucun information disponible';
+            }
+             
+      ?> 
+</div> <!-- fin container -->
