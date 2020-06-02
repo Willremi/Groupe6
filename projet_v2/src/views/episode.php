b42603b63d0da1909271ca6c27e4c63cad29fc89
Willremi <willremi@gmail.com>
2020-06-02 12:33:20 +0200

    maj episode.php

--- projet_v2/src/views/episode.php
+++ projet_v2/src/views/episode.php
@@ -1,2 +1,16 @@
 <?php
-var_dump($_GET);
+$serie = array_keys($_GET);
+$serie = $serie[0];
+// var_dump($serie);
+$serieExplode = explode('/', $serie);
+var_dump($serieExplode);
+
+$idSerie = $serieExplode[0];
+$saison = preg_split('//', $serieExplode[1], -1, PREG_SPLIT_NO_EMPTY);
+$episode = preg_split('//', $serieExplode[2], -1, PREG_SPLIT_NO_EMPTY);
+
+$saison_number = (int)$saison[1];
+$episode_number = (int)$episode[1];
+
+var_dump($saison_number);
+var_dump($episode_number);
