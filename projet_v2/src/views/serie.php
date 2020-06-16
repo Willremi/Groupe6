1d7c5c686698aacc55b7f18b220d5a4d828e1bb1
Willremi <willremi@gmail.com>
2020-06-16 13:58:29 +0200

    Update serie.php

--- projet_v2/src/views/serie.php
+++ projet_v2/src/views/serie.php
@@ -18,5 +18,32 @@ $idSerie = $_GET['id'];
       ?>
       <div class="col-sm-12 col-lg-12">
             <h3>Commentaires</h3>
+            <?php 
+            $login = $_SESSION['login'];
+            if(!$login) :
+            
+            ?>
+            <div class="">
+                <form method="POST" action="">
+                    <div class="form-group">
+                        <div class="form-group offset-md-3 col-md-6">
+                        <label for="inputPseudo">Pseudo</label>
+                        <input type="text" class="form-control" id="inputPseudo" name="inputPseudo">
+                        </div>
+                    <div class="form-group">
+                        <div class="form-group offset-md-3 col-md-6">
+                            <label for="inputMdp">Mot de passe</label>
+                            <input type="password" class="form-control" id="inputMdp" name="inputMdp">
+                        </div>
+                    </div>
+                    <div class="offset-md-6">
+                    <button type="submit" class="btn btn-primary">Valider</button>
+                    <button type="reset" class="btn btn-danger">Annuler</button>
+                    </div>
+                </form>
+                
+            </div>
+
+            <?php endif ?>
       </div>
 </div> <!-- fin container -->
