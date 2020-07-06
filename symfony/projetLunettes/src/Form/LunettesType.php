7bc15dbbb77b356c3d3d4de6643c607677e4a180
Willremi <willremi@gmail.com>
2020-07-06 16:09:14 +0200

    maj twig.yaml bootstrap

--- symfony/projetLunettes/src/Form/LunettesType.php
+++ symfony/projetLunettes/src/Form/LunettesType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Lunettes;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,13 @@ class LunettesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('prixLunettes')
+            ->add('nom', TextType::class, array(
+                'label' => 'Nom Lunettes'
+            ))
+            
+            ->add('prixLunettes', TextType::class, array(
+                'label' => 'Prix des lunettes'
+            ))
             ->add('description')
             ->add('quantite')
             ->add('statut')
