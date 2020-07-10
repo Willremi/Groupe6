f39f625095254777c59d821800094664c6d582ea
Willremi <willremi@gmail.com>
2020-07-10 09:14:34 +0200

    maj tva

--- symfony/projetLunettes/src/Form/TvaType.php
+++ symfony/projetLunettes/src/Form/TvaType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Tva;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,9 @@ class TvaType extends AbstractType
     {
         $builder
             ->add('nom')
-            ->add('prixTva')
+            ->add('prixTva', TextType::class, array(
+                'label' => 'Taux en %'
+            ))
             ->add('statut')
         ;
     }
