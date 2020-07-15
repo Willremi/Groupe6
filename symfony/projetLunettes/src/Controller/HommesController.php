210a4648125eeaaadcd69ea48d8b713e359c7969
Willremi <willremi@gmail.com>
2020-07-15 14:58:06 +0200

    maj pagination

--- symfony/projetLunettes/src/Controller/HommesController.php
+++ symfony/projetLunettes/src/Controller/HommesController.php
@@ -22,7 +22,7 @@ class HommesController extends AbstractController
         $pagination = $paginator->paginate(
             $donnees, 
             $request->query->getInt('page', 1), 
-            3
+            2
         );
         return $this->render('hommes/index.html.twig', [
             'controller_name' => 'Lunettes pour hommes', 
