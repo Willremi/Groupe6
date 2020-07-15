757d420564fb7b35fef8d0f37f638f31fbe36a77
Willremi <willremi@gmail.com>
2020-07-15 14:52:59 +0200

    maj hommes

--- symfony/projetLunettes/src/Controller/HommesController.php
+++ symfony/projetLunettes/src/Controller/HommesController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Lunettes;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HommesController extends AbstractController
@@ -10,10 +13,20 @@ class HommesController extends AbstractController
     /**
      * @Route("/hommes", name="hommes")
      */
-    public function index()
-    {
+    public function index(Request $request, PaginatorInterface $paginator)
+    {   
+        $donnees = $this->getDoctrine()->getRepository(Lunettes::class)->findBy([
+            'genre' => 1, 
+        ]);
+
+        $pagination = $paginator->paginate(
+            $donnees, 
+            $request->query->getInt('page', 1), 
+            3
+        );
         return $this->render('hommes/index.html.twig', [
-            'controller_name' => 'HommesController',
+            'controller_name' => 'Lunettes pour hommes', 
+            'pagination' => $pagination, 
         ]);
     }
 }
