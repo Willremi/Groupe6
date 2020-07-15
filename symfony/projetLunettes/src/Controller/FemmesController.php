b862d65bcbeaae391ea66d5a374542941eb3971d
Willremi <willremi@gmail.com>
2020-07-15 16:31:27 +0200

    maj femmeController

--- symfony/projetLunettes/src/Controller/FemmesController.php
+++ symfony/projetLunettes/src/Controller/FemmesController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Genre;
+use App\Entity\Lunettes;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FemmesController extends AbstractController
@@ -10,10 +14,24 @@ class FemmesController extends AbstractController
     /**
      * @Route("/femmes", name="femmes")
      */
-    public function index()
-    {
+    public function index(Request $request, PaginatorInterface $paginator)
+    {   
+        $genre = $this->getDoctrine()->getRepository(Genre::class)->findBy([
+            'nomGenre' => 'femme'
+        ]);
+
+        $donnees = $this->getDoctrine()->getRepository(Lunettes::class)->findBy([
+            'genre' => $genre, 
+        ]);
+        $pagination = $paginator->paginate(
+            $donnees, 
+            $request->query->getInt('page', 1), 
+            4
+        );
+
         return $this->render('femmes/index.html.twig', [
-            'controller_name' => 'FemmesController',
+            'controller_name' => 'Lunettes pour femmes', 
+            'pagination' => $pagination, 
         ]);
     }
 }
