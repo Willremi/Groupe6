3e209bc8c4382b1d364abd0bd3afd5216db5d1c9
Willremi <willremi@gmail.com>
2020-07-16 14:20:38 +0200

    maj CartController

--- symfony/projetLunettes/src/Controller/CartController.php
+++ symfony/projetLunettes/src/Controller/CartController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\LunettesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
@@ -10,10 +12,48 @@ class CartController extends AbstractController
     /**
      * @Route("/panier", name="cart_index")
      */
-    public function index()
-    {
+    public function index(SessionInterface $session, LunettesRepository $lunettesRepository)
+    {   
+        $panier = $session->get('panier', []);
+
+        $panierWithData = [];
+
+        foreach ($panier as $id => $quantity) {
+            $panierWithData[] = [
+                'lunettes' => $lunettesRepository->find($id), 
+                'quantity' => $quantity
+            ];
+        }
+
+        $total = 0;
+
+        foreach ($panierWithData as $item) {
+            $total += $item['lunettes']->getPrixLunettes() * $item['quantity'];
+
+        }
+
         return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+            'controller_name' => 'CartController', 
+            'items' => $panierWithData, 
+            'total' => $total, 
         ]);
+    }
+
+    /**
+     * @route("/panier/add/{id}", name="cart_add")
+     */
+    public function add($id, SessionInterface $session) {
+         
+        $panier = $session->get('panier', []);
+
+        if(!empty($panier[$id])) {
+            $panier[$id]++;
+        } else {
+            $panier[$id] = 1;
+        }
+
+        $session->set('panier', $panier);
+
+        dd($session->get('panier'));
     }
 }
