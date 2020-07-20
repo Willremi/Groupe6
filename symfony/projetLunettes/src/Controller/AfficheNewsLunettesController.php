636260664976f497ab3286229979e8aa08f75114
Willremi <willremi@gmail.com>
2020-07-20 16:36:53 +0200

    maj nouveautesLunettes

--- symfony/projetLunettes/src/Controller/AfficheNewsLunettesController.php
+++ symfony/projetLunettes/src/Controller/AfficheNewsLunettesController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Lunettes;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AfficheNewsLunettesController extends AbstractController
@@ -10,10 +13,19 @@ class AfficheNewsLunettesController extends AbstractController
     /**
      * @Route("/nouveautes_lunettes", name="nouveautes_lunettes")
      */
-    public function index()
-    {
+    public function index(Request $request, PaginatorInterface $paginator)
+    {   
+        $donnees = $this->getDoctrine()->getRepository(Lunettes::class)->findAll();
+        
+        $apgination = $paginator->paginate(
+            $donnees, 
+            $request->query->getInt('page', 1), 
+            4
+        );
+
         return $this->render('affiche_news_lunettes/index.html.twig', [
-            // 'controller_name' => 'AfficheNewsLunettesController',
+            'controller_name' => 'Dernières nouveautés du site', 
+            'pagination' => $apgination, 
         ]);
     }
 }
